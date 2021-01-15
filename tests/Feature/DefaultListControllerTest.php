3ae558753008c6c70222ee3978977978c7279b02
josgalbal <josgalbal@alum.us.es>
2021-01-15 21:15:10 +0100

    Test list default list ok

---
+++ tests/Feature/DefaultListControllerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace Tests\Feature;
+
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Faker\Generator as Faker;
+use Laravel\Passport\Passport;
+use App\User;
+
+class DefaultListControllerTest extends TestCase
+{
+
+    public function testListDefaultListsOK()
+    {
+        \Artisan::call('passport:install');
+        $this->withoutExceptionHandling();
+
+        $user = factory(User::class)->create([
+            'email' => 'secretario1@secretario1.com',
+            'password' => Hash::make('secretario1')
+        ]);
+        $this->actingAs($user, 'api');
+
+        //See Below
+        //$token = $user->generateToken();
+
+        //$headers = [ 'Authorization' => 'Bearer ' +$token];
+
+        $response = $this->get('20/api/v1/secretary/defaultlist/list');
+
+        $response->assertStatus(200);
+    }
+}
