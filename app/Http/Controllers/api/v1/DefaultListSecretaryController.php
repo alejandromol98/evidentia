b9652945af30aed4c949f065707b5fcaae1c7b84
josgalbal <josgalbal@alum.us.es>
2021-01-15 13:50:39 +0100

    New default list

--- app/Http/Controllers/api/v1/DefaultListSecretaryController.php
+++ app/Http/Controllers/api/v1/DefaultListSecretaryController.php
@@ -25,4 +25,46 @@ class DefaultListSecretaryController extends Controller
         $defaultlists = auth('api')->user()->secretary->default_lists()->get();
         return $defaultlists;
     }
+
+    /*public function create()
+    {
+        $instance = \Instantiation::instance();
+
+        $users = User::orderBy('surname')->get();
+
+        return view('defaultlist.createandedit',
+            ['instance' => $instance, 'users' => $users, 'route' => route('secretary.defaultlist.new',$instance)]);
+    } */
+
+    public function new(Request $request)
+    {
+
+        // $instance = \Instantiation::instance();
+
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'users' => 'required|array|min:1',
+        ]);
+
+        $secretary = Auth::user()->secretary;
+
+        $defaultlist = DefaultList::create([
+            'name' => $request->input('name'),
+            'secretary_id' => $secretary->id
+        ]);
+
+        $defaultlist->save();
+
+        // Asociamos los usuarios a la reunión
+        $users_ids = $request->input('users',[]);
+
+        foreach($users_ids as $user_id)
+        {
+
+            $user = User::find($user_id);
+            $defaultlist->users()->attach($user);
+
+        }
+        return $defaultlist;
+    }
 }
