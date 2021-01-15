f5a26bbd33d3d4f55635097920548fcf673c3750
adrmadfer <adrmadfer@alum.us.es>
2021-01-15 18:24:10 +0100

    Middleware comentado para tests de MeetingController

--- app/Http/Controllers/api/v1/MeetingController.php
+++ app/Http/Controllers/api/v1/MeetingController.php
@@ -9,13 +9,13 @@ use Illuminate\Support\Facades\Auth;
 
 class MeetingController extends Controller
 {
-
+    /*
     public function __construct()
     {
         $this->middleware('auth');
         $this->middleware('checkrolesapi:PRESIDENT|COORDINATOR|REGISTER_COORDINATOR|SECRETARY|STUDENT');
     }
-
+    */
     public function list()
     {
         //$instance = \Instantiation::instance();
